650d96cc10f3797900624b95627b2758cc8e78da
Robrt <raaguilarm@gmail.com>
2020-06-26 05:43:00 -0500

    Pag principal add

---
+++ public/javascripts/map.js
@@ -0,0 +1,22 @@
+var mymap = L.map('main_map').setView([9.02338722 , -79.53186929], 13);
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors '
+}).addTo(mymap);
+
+
+
+L.marker([9.02338722,-79.53186929]).addTo(mymap);
+L.marker([9.02196734,-79.53299314]).addTo(mymap);
+//L.marker([9.02338722,-79.53186929]).addTo(mymap);
+
+
+// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: 'your.mapbox.access.token'
+// }).addTo(mymap);
